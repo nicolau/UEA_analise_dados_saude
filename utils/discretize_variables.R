3bc3e7fddb7e9f1cf07e4d2fa0ce4fb925e35cdd
André Nicolau <anicolau85@gmail.com>
2019-10-16 15:59:24 -0300

    Update discretize_variables.R

--- utils/discretize_variables.R
+++ utils/discretize_variables.R
@@ -1,9 +1,20 @@
-install.packages("arules")
+# install.packages("arules")
 library(arules)
 
 DF <- data.frame(
-  idade = rnorm(100, 30, 10)
+  idade = as.integer(rnorm(100, 30, 10))
 )
 
 summary(DF)
-DF$idadeFactor <- discretize(x = DF$idade, method = "fixed", breaks = c(0, 10, 30, 50), labels = c(1, 2, 3))
+
+discretize(x = DF$idade)
+
+DF$faixa <- discretize(x = DF$idade,
+                       method = "fixed",
+                       breaks = c(0, 18, 40, 60, 120))
+
+DF$cat <- discretize(x = DF$idade,
+                     method = "fixed",
+                     breaks = c(0, 18, 40, 60, 120),
+                     labels = c("crianca", "adulto", "experiente", "imortal"))
+
